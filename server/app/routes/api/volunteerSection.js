e391c9ee4c4c5ef5cd10f7ea15dd1600765efe15
landrew831 <landrew831@gmail.com>
2018-09-06 19:55:58 -0400

    volunteer section

---
+++ server/app/routes/api/volunteerSection.js
@@ -0,0 +1,126 @@
+router = require('express').Router()
+const VolunteerSection = require('../../models/volunteerSection.models');
+
+router.get('/', (req, res) => {
+    VolunteerSection.find().then( volunteerSections => {
+        res.send(volunteerSections)
+    }).catch(err => {
+        res.status(500).send({
+            message: err.message || "Some error occured while retrieving volunteer sections."
+        });
+    });
+});
+
+router.get('/:id', (req, res) => {
+    if (!req.params.id) {
+        return res.status(400).send({
+            message: "A volunteer section is required to find the section"
+        });
+    };
+    VolunteerSection.findOne({ 'id' : req.params.id }).then(volunteerSection => {
+        if (!volunteerSection) {
+            return res.status(404).send({
+                message: "Volunteer section not found with id " + req.params.id
+            });
+        };
+        res.send(volunteerSection);
+    }).catch(err => {
+        if (err.kind === "ObjectId") {
+            return res.status(404).send({
+                message: "Volunteer section not found with id " + req.params.id
+            });
+        }
+        return res.status(500).send({
+            message: "There was a problem retrieving volunteer section " + req.params.id
+        });
+    });
+});
+
+router.post('/', (req, res) => {
+    if (!(req.body.event && req.body.date && req.body.location &&
+        req.body.volunteerCoordinator)) {
+
+        // error message needs to indicate which field(s) are missing
+        return res.status(400).send({
+            message: "All required fields must be present."
+        });
+    };
+
+    if (VolunteerSection.findOne({'id': req.body.id}).then(volunteerSection => {
+        if (volunteerSection) {
+            return res.status(400).send({
+                message: "A volunteer section with this username already exists." + req.body.id
+            });
+        } else {
+        const volunteerSection = new VolunteerSection({
+            event: req.body.event,
+            date: req.body.date,
+            location: req.body.location,
+            volunteerCoordinator: req.body.volunteerCoordinator
+        })
+
+        user.save().then(data => {
+            res.send(data);
+        }).catch(err => {
+            res.status(500).send({
+                message: err.message
+            })
+        })
+        }
+    }).catch(err => {
+        return res.status(500).send({
+            message: "Internal server error"
+        });
+    }));
+});
+
+router.put('/:id', (req, res) => {
+    if (!req.params.id) {
+        return res.status(400).send({
+            message: "A valid id must be provided to update the volunteer section."
+        });
+    };
+
+    VolunteerSection.findOneAndUpdate({id: req.params.id}, {
+      event: req.body.event,
+      date: req.body.date,
+      location: req.body.location,
+      volunteerCoordinator: req.body.volunteerCoordinator
+    }, {new: true}).then(volunteerSection => {
+        if (!volunteerSection) {
+            return res.status(404).send({
+                message: "Volunteer section not found with id " + req.params.id
+            })
+        };
+        res.send(volunteerSection);
+    }).catch(err => {
+        if (err.kind === 'ObjectId') {
+            return res.status(404).send({
+                message: "Volunteer section not found with id " + req.params.id
+            });
+        };
+    });
+});
+
+router.delete('/:id', (req, res) => {
+    VolunteerSection.findOneAndRemove({id: req.params.id})
+    .then(volunteerSection => {
+        if(!volunteerSection) {
+            return res.status(404).send({
+                message: "Volunteer section not found with username " + req.params.id
+            });
+        }
+        res.send({message: "Volunteer section deleted successfully!"});
+    }).catch(err => {
+        if(err.kind === 'ObjectId' || err.name === 'NotFound') {
+            return res.status(404).send({
+                message: "Volunteer section not found with id " + req.params.id
+            });
+        }
+        return res.status(500).send({
+            message: "Could not delete volunteer section with id " + req.params.id
+        });
+    });
+});
+
+module.exports = router;
