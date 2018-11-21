d961511cd33399b9d2e54c6d6e8ca399e0d9e64e
Rakuram, Sidartha <sidartha@gatech.edu>
2018-11-21 17:37:21 -0500

    Change components extension to .jsx
    This will allow IDEs/code editors to recognize that they are React code

--- client/src/components/Resources.jsx
+++ client/src/components/Resources.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ReactDOM from 'react-dom';
 import ReactMarkdown from 'react-markdown';
 
 let input = "# Resources for SCPCers\n\n**Volunteer Sheet**: https://docs.google.com/spreadsheets/d/1AjOm6V3ZG0Jn0y__CR0vxmwXHJp3bOaETq18uPECrac/edit#gid=1777936343\n\n**Marketing form**: https://goo.gl/forms/RjtusxgGqmRm3q6L2\n\n**Points Form**: https://docs.google.com/forms/d/e/1FAIpQLSdXGkAl5uTXE952FiV_XuRejOOQaCx8PnNc_f7iLag6X1U_0A/viewform?usp=sf_link\n\n**Asana**: https://asana.com";
