74dcea24321ebe724b019c34a178cdaa0493d21b
BitLeigh Vu <bitleighvuu@gmail.com>
2019-02-01 10:59:54 -0500

    removed resources on the home page

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
-import logo from '../images/logo.svg';
 import './App.css';
-import Resources from "./Resources";
 import Header from './Header';
 import Footer from './Footer'; 
+import Home from './Home'; 
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-        <Resources />
+        <Home />
         <Footer />
       </div>
     );
