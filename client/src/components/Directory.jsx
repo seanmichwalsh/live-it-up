628091b3bf2c3f1fefb554413b5450872f8a0691
kinathan <knathan7@gatech.edu>
2019-02-06 14:17:34 -0500

    Directory Page

---
+++ client/src/components/Directory.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+class Directory extends Component {
+
+const users = [1 2 3 4 5]
+
+    render() {
+        return (
+            <div>
+           		column-count: 6;
+          			<h1 className="Directory-title">
+          				Directory
+          			</h1>
+            		<p className = "Directory">
+						const users = numbers.map((user) =>
+  							//<li> <img src="scpc.png" alt="{user.name}"> </li>
+  							<li>Name: {user}</li>
+  							//<li>Email: {user.email}</li>
+  							//<li>Committee: {user.committee}</li>
+						);
+					</p>
+            </div>
+        );
+    }
+}
+
+export default Directory;
