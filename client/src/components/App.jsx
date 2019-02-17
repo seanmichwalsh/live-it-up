5fe9202bbba5389d1ade1b71781faa453e86fe34
Sidartha Rakuram <sidartha@gatech.edu>
2019-02-16 19:32:01 -0500

    Add React Router for routing

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -3,16 +3,26 @@ import './App.css';
 import Header from './Header';
 import Footer from './Footer'; 
 import Home from './Home'; 
+import User from './User'; 
 
+import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'; 
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
-        <Home />
-        <Footer />
-      </div>
+        <BrowserRouter>
+          <div className="App">
+            <Header />
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path='/directory' component={User} />
+              {/* We should add other Routes here */}
+            </Switch>
+            <Footer />
+          </div>
+          
+        </BrowserRouter>
+        
     );
   }
 }
