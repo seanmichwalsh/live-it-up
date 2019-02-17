7f8563196d6c3c744d552c7929e8a1f3dfdc9325
Sidartha Rakuram <sidartha@gatech.edu>
2019-02-16 19:32:01 -0500

    Add React Router for routing

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,38 +1,40 @@
 import React from 'react';
 import './Header.css';
 
+import { Link } from 'react-router-dom';
+
 export default class Header extends React.Component {
     constructor(props) {
       super(props);
     }
 
-render() {
-    return (
-        <div class="header">
-            <img src={process.env.PUBLIC_URL + "/images/SCPCHeader.jpg"} alt="scpc logo"></img>
-                <nav class="navbar navbar-expand-sm navbar-fnt sticky-top">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link" href="/components/home">HOME</a>
-                        </li> 
-                        <li class="nav-item">
-                            <a class="nav-link"href="/components/calendar">CALENDAR</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/components/volunteer">VOLUNTEER</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link"href="/components/points">POINTS</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link"href="/components/directory">DIRECTORY</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="/components/resources">RESOURCES</a>
-                        </li>
-                    </ul>
-                </nav>
-        </div>
-    );
+    render() {
+        return (
+            <div className="header">
+                <img src={process.env.PUBLIC_URL + "/images/SCPCHeader.jpg"} alt="SCPC Logo"></img>
+                    <nav className="navbar navbar-expand-sm navbar-fnt sticky-top">
+                        <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/">HOME</Link>
+                            </li> 
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/calendar">CALENDAR</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/calendar">VOLUNTEER</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/points">POINTS</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/directory">DIRECTORY</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/resources">RESOURCES</Link>
+                            </li>
+                        </ul>
+                    </nav>
+            </div>
+        );
     }
 }
