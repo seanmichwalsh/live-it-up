bbfab0916b4b097b926f49d9a9ae9626be77f9fc
Marissa Yun <marissayun27@gmail.com>
2019-03-07 18:52:43 -0500

    Added Event page

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -4,6 +4,7 @@ import Header from './Header';
 import Footer from './Footer'; 
 import Home from './Home'; 
 import User from './User'; 
+import Events from './Events';
 
 import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'; 
 
@@ -16,6 +17,7 @@ class App extends Component {
             <Switch>
               <Route exact path="/" component={Home} />
               <Route exact path='/directory' component={User} />
+              <Route exact path='/events' component={Events} />
               {/* We should add other Routes here */}
             </Switch>
             <Footer />
