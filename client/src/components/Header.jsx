4436ce118c4d01e3a9d6976d37ada8966d197773
BitLeigh Vu <bitleighvu@gatech.edu>
2019-03-10 13:43:09 -0400

    responsive navbar for mobile and desktop view

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -12,7 +12,7 @@ export default class Header extends React.Component {
         return (
             <div className="header">
                 <img src={process.env.PUBLIC_URL + "/images/SCPCHeader.jpg"} alt="SCPC Logo"></img>
-                    <nav className="navbar navbar-expand-sm navbar-fnt sticky-top">
+                    {/* <nav className="navbar navbar-expand-sm navbar-fnt sticky-top">
                         <ul className="navbar-nav">
                             <li className="nav-item">
                                 <Link className="nav-link" to="/">HOME</Link>
@@ -33,7 +33,41 @@ export default class Header extends React.Component {
                                 <Link className="nav-link" to="/resources">RESOURCES</Link>
                             </li>
                         </ul>
-                    </nav>
+                    </nav> */}
+
+                <nav class="navbar navbar-expand-sm navbar-light bg-white">
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarToggler" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarToggler">
+                    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                    <ul className="navbar-nav">
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/">HOME</Link>
+                            </li> 
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/calendar">CALENDAR</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/calendar">VOLUNTEER</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/points">POINTS</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/directory">DIRECTORY</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/resources">RESOURCES</Link>
+                            </li>
+                        </ul>
+                    </ul>
+                </div>
+                </nav>
+                
+
+                
             </div>
         );
     }
