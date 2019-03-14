92ecfc0db93b5c84f52b62b0ae312ed6ca2ec30b
Nathan, Kishore <knathan7@gatech.edu>
2019-03-14 19:13:43 -0400

    Directory Page (#37)
    * Directory Page
    
    * Directory
    
    * directory

---
+++ client/src/components/Directory.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react';
+import logo from '../images/logo.svg';
+import ReactDOM from 'react-dom';
+import './Directory.css'
+
+class Directory extends Component {
+
+users = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
+
+
+    render() {
+        return (
+            <div>
+          			<h1 className="title">
+          				Directory
+          			</h1>
+            		<p className = "list">
+						
+						{this.users.map((user) =>
+						<div>
+  							<ul> <img src={logo} alt={user}/></ul>
+  							<ul> Name: {user}</ul>
+  							<ul>Email: {user}</ul>
+  							<ul>Committee: {user}</ul>
+  						</div>
+						)}
+					</p>
+            </div>
+        );
+    }
+}
+
+export default Directory;
