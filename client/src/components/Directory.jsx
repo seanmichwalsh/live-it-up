f60ab61d24a457ba5b5cb4657a88e4c9e686a407
BitLeigh Vu <bitleighvu@gatech.edu>
2019-03-24 22:32:43 -0400

    add/edit box for users and committees

--- client/src/components/Directory.jsx
+++ client/src/components/Directory.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import logo from '../images/logo.svg';
+import { BrowserRouter, Route, Link, Switch, withRouter } from 'react-router-dom'; 
 import ReactDOM from 'react-dom';
 import './Directory.css'
 
@@ -7,11 +8,28 @@ class Directory extends Component {
 
 users = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
     render() {
+		
+
         return (
             <div className="directory-page">
-          			<h1 className="title">
-          				Directory
-          			</h1>
+          			<div className="top-bar">
+                    <header className="header">DIRECTORY</header>
+                    <div className="add-edit-box">
+						<div class="dropdown">
+							<button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+								ADD/EDIT
+							</button>
+							<div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+								<a class="dropdown-item" href="/adduser">Add User</a>
+								<a class="dropdown-item" href="/edituser">Edit User</a>
+								<a class="dropdown-item" href="/addcommittee">Add Committee</a>
+								<a class="dropdown-item" href="/editcommittee">Edit Committee</a>
+							</div>
+						</div>
+                    </div>
+                </div>
+                <div className="committee-name">TECHNOLOGY</div>
+
             		<p className = "list">
 						
 						{this.users.map((user) =>
