32cf2f8bd5d00804ec1b99b1e80d9013bd57b710
BitLeigh Vu <bitleighvuu@gmail.com>
2019-03-24 22:14:14 -0400

    change classname

--- client/src/components/Directory.jsx
+++ client/src/components/Directory.jsx
@@ -6,8 +6,6 @@ import './Directory.css'
 class Directory extends Component {
 
 users = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-
-
     render() {
         return (
             <div className="directory-page">
