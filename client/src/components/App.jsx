0625e051c294808647fb6abd13220832e4cdbbf7
BitLeigh Vu <bitleighvu@gatech.edu>
2019-04-29 13:48:09 -0400

    add points to router

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -7,7 +7,7 @@ import Footer from './Footer';
 import Home from './Home'; 
 import AddUser from './AddUser';
 import Calendar from './Calendar';
-
+import Points from './Points';
 
 import { BrowserRouter, Route, Link, Switch } from 'react-router-dom'; 
 
@@ -23,6 +23,7 @@ class App extends Component {
               <Route exact path='/resources' component={Resources} />
               <Route exact path="/adduser" component={AddUser} />
               <Route exact path="/calendar" component={Calendar} />
+              <Route exact path="/points" component={Points} />
               {/* We should add other Routes here */}
             </Switch>
             <Footer />
