b5a16186c8b0d40ffe75002dc0a98dce6fd526b1
Vu, Bitleigh Ngoc <bitleighvu@gatech.edu>
2019-06-11 16:31:02 -0700

    Directory (#43)
    * Directory Page
    
    * Directory
    
    * directory
    
    * add css changes
    
    * change classname
    
    * fix incorrect route for directory
    
    * add/edit box for users and committees
    
    * add add/edit bar
    
    * responsive media view
    
    * inline dropdown
    
    * remove ul styling
    
    * redoing placement of user
    
    * responsive formatting
    
    * center directory header
    
    * remove footer from directory branch
    
    * mobile responsive changes
    
    * added font formula
    
    * add new pages (same as edit pages)
    
    * change color of add button
    
    * new add user route
    
    * added values to dropdown
    
    * edit user only accessbile by clicking particular 1
    
    * remove not needed imports. change button name
    
    * add new committee page
    
    * add committee page as a route
    
    * change quotation marks
    
    * remove home
    
    * remove form and add filler text
    
    * add form validation func
    
    * add space to end of page
    
    * remove uneeded js
    
    * function
    
    * add validation inputs are filled out
    
    * remove filler page
    
    * add pattern regex to inputs

--- client/src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import "./Footer.css"
-
-const Footer = () => {
-  return <footer className="footer"> Copyright &copy; SCPC, Georgia Tech </footer>;
-};
-
-export default Footer;
