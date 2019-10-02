f9ba007c3a6b7ea245ce26df09cbc25c2d7a1dce
BitLeigh Vu <bitleighvu@gatech.edu>
2019-10-02 02:40:08 -0400

    not fully correct points display

--- client/src/components/Points/Points.jsx
+++ client/src/components/Points/Points.jsx
@@ -1,87 +1,85 @@
-import React, { Component } from 'react';
+import React, { useEffect } from 'react';
 import $ from 'jquery';
 import './Points.css';
 import points from "./test.json";
+import { PropTypes } from "prop-types";
+import { getUser } from "../../redux/actions/userActions";
+import { getPoints } from '../../redux/actions/pointsActions';
+import { connect } from "react-redux";
 
-class Points extends Component {
-
-  render() {
-    var sum = 0.0;
-    var datas = points.data.schema;
-    console.log(datas);
-    console.log(Object.keys(datas));
-    const galList = Object.keys(datas).map((item, index) => 
-      <tr className="tr-className-1" data-title="bootstrap table">
-        <td className="td-className-1" data-title="bootstrap table">
-          <a className="date">{datas[item].semester}</a>
-        </td>
-        <td classname="points">{datas[item].group1}</td>
-        <td className="committee">{datas[item].group2}</td>
-        <td className="category">{datas[item].group3}</td>
-        <td className="description">{datas[item].committee}</td>
-      </tr>
-      );
+const Points = ({ getUser , user, getPoints }) => {
+    
+  useEffect(() => {
+      getUser();
+      getPoints();
+    }, [user]);
 
     return (
       <div className="directory-page">
-      <div className="top-bar">
-        <header id="header">
-          <div id="header-text">POINTS</div>
+        <div className="top-bar">
+          <header id="header">
+            <div id="header-text">POINTS</div>
 
-          <div id="add-edit-box">
-            <div className="dropdown">
-            <a href="/addpoints"
-              class="btn btn-secondary btn-small active"
-              role="button"
-              aria-pressed="true">
-                ADD
-            </a>
+            <div id="add-edit-box">
+              <div className="dropdown">
+              <a href="/addpoints"
+                class="btn btn-secondary btn-small active"
+                role="button"
+                aria-pressed="true">
+                  ADD
+              </a>
+              </div>
             </div>
-          </div>
-        </header>
-      </div>
-      
-
-      
+          </header>
+        </div>
+        <div id="mainPG">
+            <div id="points-display">
+                <div id="display-header">{user.firstName} Total Points: </div>
+                <div id="user-table">
+                  <table data-toggle="table" className="table table-bordered">
+                      <thead>
+                        <tr>
+                          <th>Semester</th>
+                          <th>Category 1 Points</th>
+                          <th>Category 2 Points</th>
+                          <th>Category 3 Points</th>
+                          <th>Committee Points</th>
+                          <th>Ad Hoc</th>
+                          <th>Office Hours</th>
+                          <th>Teasering</th>
+                        </tr>
+                      </thead>
+                      <tbody>
+                        {/* map user points to specific heading */}
+                      </tbody>
+                    </table>
+                </div>
+            </div>
+            <div id="points-sidebar">
+                <div id="req-header">REQUIREMENTS</div>
+                {/* depending on calculations of each point category
+                    display different color boxes to determine if member
+                    is in good standing... follow mock-ups */}
+            </div>
+        </div>
+    
     </div>
   );
 };
-//       <div id="points-pg">
-//         <header id="header">
-//           <a id="header-text">POINTS</a>
-//           <a href="/addpoints"
-//             class="btn btn-secondary btn-small active"
-//             role="button"
-//             aria-pressed="true">
-//               ADD
-//           </a>
-//         </header> 
-//         <div id="mainPG">
-//             <div id="points-display">
-//                 <div id="display-header">Sidartha's Total Points: </div>
-//                 <div id="user-table">
-//                   <table data-toggle="table" className="table table-bordered">
-//                       <thead>
-//                         <tr>
-//                           <th>Semester</th>
-//                           <th>Category 1 Points</th>
-//                           <th>Category 2 Points</th>
-//                           <th>Category 3 Points</th>
-//                           <th>Committee Points</th>
-//                         </tr>
-//                       </thead>
-//                       <tbody>
-//                         {galList}
-//                       </tbody>
-//                     </table>
-//                 </div>
-//             </div>
-//             <div id="points-sidebar">
-//                 <div id="req-header">REQUIREMENTS</div>
-//             </div>
-//         </div>
-//       </div>
-//   );
-// };
+
+Points.propTypes = {
+  getUser: PropTypes.func.isRequired,
+  user: PropTypes.object.isRequired,
+  getPoints: PropTypes.func.isRequired
 }
-export default Points;
+
+const mapStateToProps = state => ({
+  user: state.user,
+  current: state.current,
+  points: state.points
+});
+
+export default connect(
+  mapStateToProps,
+  { getUser, getPoints }
+)(Points);
