568f68abd52606facc2127cc07fe97622859d07b
zkang35 <harrydexiong@gmail.com>
2019-10-02 21:02:03 -0400

    Partially functioning pointNew

--- client/src/components/Points/AddPoints.jsx
+++ client/src/components/Points/AddPoints.jsx
@@ -1,12 +1,8 @@
-import React, { useState, useEffect } from "react";
-import $ from "jquery";
-import { ToastContainer, toast } from "react-toastify";
+import React from "react";
+import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
-import { connect } from "react-redux";
 import "./AddPoints.css";
-import { PropTypes } from "prop-types";
-import { getUsers } from "./../../redux/actions/userActions";
-import AddPointsForm from './AddPointsForm';
+import AddPointsForm from "./AddPointsForm";
 
 const AddPoints = () => (
   <div className="add-user-pg">
@@ -18,17 +14,4 @@ const AddPoints = () => (
   </div>
 );
 
-AddPoints.propTypes = {
-  addPoint: PropTypes.func.isRequired,
-  users: PropTypes.array.isRequired
-};
-
-const mapStateToProps = state => ({
-  users: state.user.users
-});
-
-export default connect(
-  mapStateToProps,
-  { AddPoints, getUsers }
-)(AddPoints);
-
+export default AddPoints;
