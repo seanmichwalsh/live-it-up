c3c672a03d099310a74e7796c778c2888c72a330
zkang35 <harrydexiong@gmail.com>
2019-10-14 11:33:15 -0400

    Time to Date conversion

--- client/src/components/Calendar/AddEventForm.jsx
+++ client/src/components/Calendar/AddEventForm.jsx
@@ -7,49 +7,61 @@ import "react-toastify/dist/ReactToastify.css";
 import { addEvent } from "../../redux/actions/eventActions";
 import { getCommittees } from "../../redux/actions/committeeActions";
 
-const AddEventForm = ({ addEvent }) => {
+const AddEventForm = ({ addEvent, committees, getCommittees }) => {
   const [name, setName] = useState("");
   const [location, setLocation] = useState("");
-  const [committees, setCommittee] = useState("");
-  const [date, setDate] = useState("");
+  const [committee, setCommittee] = useState([]);
+  const [date, setDate] = useState(new Date());
   const [startTime, setStartTime] = useState("");
   const [endTime, setEndTime] = useState("");
 
   useEffect(() => {
     getCommittees();
-  }, [committees]);
+  }, [committee]);
 
   const onSubmit = e => {
     e.preventDefault();
-    if (name === "" || location === "" 
-        || date === null || startTime === null || endTime === null) {
+    if (
+      name === "" ||
+      location === "" ||
+      date === null ||
+      startTime === null ||
+      endTime === null
+    ) {
       toast("Please fill in every required field!");
     } else {
-
       // time manipulation to send to api
       // new Date(year, month, date, hours, minutes, seconds, ms)
       var newDate = new Date(date);
-      var newStartTime = new Date(startTime);
-      var newEndTime = new Date(endTime);
-      var newStart = new Date(newDate.getYear(), newDate.getMonth(), newDate.getDate());
-      var newEnd = new Date(newDate.getYear(), newDate.getMonth(), newDate.getDate());
-
+      var newStart = new Date(
+        newDate.getYear(),
+        newDate.getMonth(),
+        newDate.getDate(),
+        startTime.substring(0, 2),
+        startTime.substring(3)
+      );
+      var newEnd = new Date(
+        newDate.getYear(),
+        newDate.getMonth(),
+        newDate.getDate(),
+        endTime.substring(0, 2),
+        endTime.substring(3)
+      );
       const newEvent = {
         name: name,
         location: location,
-        // committees: committees,
-        startTime: date,
-        endTime: date
+        committees: committee,
+        startTime: newStart,
+        endTime: newEnd
       };
       addEvent(newEvent);
-      
       //Clear Fields
       setName("");
       setLocation("");
       setCommittee("");
-      setDate(null);
-      setStartTime(null);
-      setEndTime(null);
+      setDate(new Date());
+      setStartTime(new Date());
+      setEndTime(new Date());
     }
   };
 
@@ -87,55 +99,58 @@ const AddEventForm = ({ addEvent }) => {
           <select
             id="committees"
             className="form-control"
-            onChange={e => setCommittee(e.target.value)}
+            onChange={e => {
+              committee.push(e.target.value);
+              setCommittee(committee);
+            }}
           >
-            <option defaultValue disabled value="">
+            <option selected disabled value="">
               Choose...
             </option>
-            {/* {committees
-              .filter(committee => committee.type === "Primary")
-              .map(committee => (
-                <option key={committee._id} value={committee._id}>
-                  {committee.name}
-                </option>
-              ))} */}
+            {committees !== null &&
+              committees
+                .filter(committee => committee.type === "Primary")
+                .map(committee => (
+                  <option key={committee._id} value={committee._id}>
+                    {committee.name}
+                  </option>
+                ))}
           </select>
         </div>
         <div className="form-group col-md-3 text-left">
-            <label htmlFor="type">Date</label>
-            <input
-                type="date"
-                className="form-control"
-                id="type"
-                placeholder="2019-10-23"
-                onChange={e => setDate(e.target.value)}
-                value={date}
-                required
-            />
-            </div>
-            <div className="form-group col-md-3 text-left">
-            <label htmlFor="type">Start Time</label>
-            <input
-                type="time"
-                className="form-control"
-                id="type"
-                onChange={e => setStartTime(e.target.value)}
-                value={startTime}
-                required
-            />
-            </div>
-            <div className="form-group col-md-3 text-left">
-            <label htmlFor="type">End Time</label>
-            <input
-                type="time"
-                className="form-control"
-                id="type"
-                placeholder="9:00"
-                onChange={e => setEndTime(e.target.value)}
-                value={endTime}
-                required
-            />
-            </div>
+          <label htmlFor="type">Date</label>
+          <input
+            type="date"
+            className="form-control"
+            id="type"
+            onChange={e => setDate(e.target.value)}
+            value={date}
+            required
+          />
+        </div>
+        <div className="form-group col-md-3 text-left">
+          <label htmlFor="type">Start Time</label>
+          <input
+            type="time"
+            className="form-control"
+            id="type"
+            onChange={e => setStartTime(e.target.value)}
+            value={startTime}
+            required
+          />
+        </div>
+        <div className="form-group col-md-3 text-left">
+          <label htmlFor="type">End Time</label>
+          <input
+            type="time"
+            className="form-control"
+            id="type"
+            placeholder="9:00"
+            onChange={e => setEndTime(e.target.value)}
+            value={endTime}
+            required
+          />
+        </div>
       </div>
       <button
         type="submit"
@@ -150,8 +165,9 @@ const AddEventForm = ({ addEvent }) => {
 };
 
 AddEventForm.propTypes = {
-  // addEvents: PropTypes.func.isRequired,
-  committees: PropTypes.array.isRequired
+  addEvents: PropTypes.func.isRequired,
+  committees: PropTypes.array.isRequired,
+  getCommittees: PropTypes.func.isRequired
 };
 
 const mapStateToProps = state => ({
