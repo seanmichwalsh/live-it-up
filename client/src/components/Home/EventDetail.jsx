3fd1801dbf18f8c788356c72ab5d5c3029becfe5
BitLeigh Vu <bitleighvuu@gmail.com>
2020-04-23 19:53:24 -0400

    fix home responsiveness and connect with database

--- client/src/components/Home/EventDetail.jsx
+++ client/src/components/Home/EventDetail.jsx
@@ -1,18 +1,13 @@
 import React, { Component } from 'react';
-import './EventDetail.css';
+// import './EventDetail.css';
 
 class EventDetail extends Component {
-
     render() {
         return (
-            <div className="col">
-                <div className="col">
-                    <img
-                        src={this.props.URL}
-                        alt={this.props.alt}
-                        className="responsive-img"
-                    ></img>
-                </div>
+            <div className="eventDetail">
+                <img
+                    src={this.props.URL}
+                ></img>
                 <div className="description">
                     Date: {this.props.date} <br></br>
                     Time: {this.props.time} <br></br>
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,60 +1,57 @@
-import React, { Component } from 'react';
+import React, { useEffect, useState } from 'react';
+import { connect } from "react-redux";
+import PropTypes from "prop-types";
+
+import { getEvents } from '../../redux/actions/eventActions';
+
 import SideBar from './SideBar';
 import EventDetail from './EventDetail';
 import './Home.css';
 
-class Home extends Component {
-  
-  constructor(props) {
-    super(props);
-  }  
+const Home = ({ getEvents, events }) => {
+  const [ event, setEvent ] = useState([]);
 
-  render() {
-    return (
-      <div className="entire-page">
-        <div className="main-page">
-          <div className="main-header">Upcoming Events</div>
-          <div class="container">
-            <div class="row">
+  useEffect(() => {
+    getEvents();
+  }, []);
+
+  useEffect(() => {
+    events.sort(function(a, b) {
+      var dateA = new Date(a.startTime), dateB = new Date(b.startTime);
+      return dateA - dateB;
+    });
+    setEvent(events);
+  }, [events]);
+
+  return (
+    <div className="entire-page">
+      <div className="main-page">
+        <div className="main-header">Upcoming Events</div>
+            {events.map((event) => 
               <EventDetail 
-                URL={process.env.PUBLIC_URL + "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/91873401_10159808570233066_8270082192713973760_o.jpg?_nc_cat=103&_nc_sid=b386c4&_nc_ohc=p1BHxMzZBAEAX8MXlSW&_nc_ht=scontent-lga3-1.xx&oh=651d2f661db11f7086e3f2471429c3d9&oe=5EB67789"}
-                alt={"hello"}
-                date={"hello"}
-                time={"hello"}
-                location={"hello"}
-              />
-            </div>
-            <div class="row">
-              <EventDetail 
-                URL={process.env.PUBLIC_URL + "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/91583754_10159806681788066_1479397022636179456_o.jpg?_nc_cat=106&_nc_sid=b386c4&_nc_ohc=p4M2KWithEAAX8WXSQc&_nc_ht=scontent-lga3-1.xx&oh=5efbaf2f05b1004f211996eb0f339f8b&oe=5EB6CF03"}
-                alt={"hello"}
-                date={"hello"}
-                time={"hello"}
-                location={"hello"}
-              />
-              <EventDetail 
-                URL={process.env.PUBLIC_URL + "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/92781522_10159835536373066_6090730398478761984_o.jpg?_nc_cat=102&_nc_sid=b386c4&_nc_ohc=drMr1CveAMEAX8SY46X&_nc_ht=scontent-lga3-1.xx&oh=369a5d8588dfdc39350bed3d52d3d7c4&oe=5EB6E3FD"}
-                alt={"hello"}
-                date={"hello"}
-                time={"hello"}
-                location={"hello"}
-              />
-              <EventDetail 
-                URL={process.env.PUBLIC_URL + "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/92455259_10159830710513066_410111927285448704_o.jpg?_nc_cat=103&_nc_sid=b386c4&_nc_ohc=bCTX0APy8HUAX_ctF-_&_nc_ht=scontent-lga3-1.xx&oh=e477e48910e3303ba017c32834f2d462&oe=5EB62EDE"}
-                alt={"hello"}
-                date={"hello"}
-                time={"hello"}
-                location={"hello"}
-              />
-            </div>
-          </div>
-        </div>  
-        <div className="side-bar">
-          <SideBar />
-        </div>
+              URL={process.env.PUBLIC_URL + "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/91873401_10159808570233066_8270082192713973760_o.jpg?_nc_cat=103&_nc_sid=b386c4&_nc_ohc=p1BHxMzZBAEAX8MXlSW&_nc_ht=scontent-lga3-1.xx&oh=651d2f661db11f7086e3f2471429c3d9&oe=5EB67789"}
+              date={event.eventName}
+              time={event.startTime}
+              location={event.location}
+              className="event"
+            />
+            )}
+      </div>  
+
+      <div className="side-bar">
+        <SideBar />
       </div>
-    );
-  }
+    </div>
+  );
 };
 
-export default Home;
+Home.propTypes = {
+  getEvents: PropTypes.func.isRequired,
+  events: PropTypes.array.isRequired
+};
+
+const mapStateToProps = state => ({
+  events: state.event.events
+});
+
+export default connect(mapStateToProps, { getEvents })(Home);
