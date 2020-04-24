107262ecaf449b83ea506f14905612e6db22caee
BitLeigh Vu <bitleighvu@gatech.edu>
2020-04-24 01:14:42 -0400

    new eventDetail comp

--- client/src/components/Home/EventDetail.jsx
+++ client/src/components/Home/EventDetail.jsx
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
-// import './EventDetail.css';
+import './EventDetail.css';
 
 class EventDetail extends Component {
     render() {
         return (
             <div className="eventDetail">
-                <img
+                <img className="pic"
                     src={this.props.URL}
                 ></img>
                 <div className="description">
-                    Date: {this.props.date} <br></br>
-                    Time: {this.props.time} <br></br>
-                    Location: {this.props.location}
+                    <div>Date: {this.props.date}</div>
+                    <div>Time: {this.props.time} </div>
+                    <div>Location: {this.props.location}</div>
                 </div>
             </div> 
         );
