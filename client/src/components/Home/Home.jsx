13ca647fa1378eaf44e1a2939ab8a01e656c5f18
BitLeigh Vu <bitleighvuu@gmail.com>
2020-04-24 01:14:55 -0400

    flex box container

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -27,17 +27,17 @@ const Home = ({ getEvents, events }) => {
     <div className="entire-page">
       <div className="main-page">
         <div className="main-header">Upcoming Events</div>
+          <div id="events-container">
             {events.map((event) => 
               <EventDetail 
-              URL={process.env.PUBLIC_URL + "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/91873401_10159808570233066_8270082192713973760_o.jpg?_nc_cat=103&_nc_sid=b386c4&_nc_ohc=p1BHxMzZBAEAX8MXlSW&_nc_ht=scontent-lga3-1.xx&oh=651d2f661db11f7086e3f2471429c3d9&oe=5EB67789"}
-              date={event.eventName}
-              time={event.startTime}
-              location={event.location}
-              className="event"
-            />
+                URL={process.env.PUBLIC_URL + "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/91873401_10159808570233066_8270082192713973760_o.jpg?_nc_cat=103&_nc_sid=b386c4&_nc_ohc=p1BHxMzZBAEAX8MXlSW&_nc_ht=scontent-lga3-1.xx&oh=651d2f661db11f7086e3f2471429c3d9&oe=5EB67789"}
+                date={event.eventName}
+                time={event.startTime}
+                location={event.location}
+              />
             )}
+          </div>
       </div>  
-
       <div className="side-bar">
         <SideBar />
       </div>
