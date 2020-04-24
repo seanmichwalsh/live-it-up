8c7a4c23ba141c32c70a88ba8ffaec900087a85d
Kang, Zixuan <zkang35@gatech.edu>
2020-04-23 22:02:27 -0400

    Cleared Up Points.jsx so that evrything is modualized

--- client/src/components/Points/MemberView.jsx
+++ client/src/components/Points/MemberView.jsx
@@ -1,46 +1,161 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
+import "react-toastify/dist/ReactToastify.css";
+import "./MemberView.css";
+import { connect } from "react-redux";
+import { getPointsReportForUser } from "../../redux/actions/pointsActions";
 
-export const MemberView = ({ title, header, details }) => {
-  return (
-    <div className="directory-page">
-      <div className="top-bar">
-        <header id="header">
-          <div id="header-text">POINTS</div>
-          <div id="add-edit-box">
-            <div className="dropdown">
-              <a
-                href="/addpoints"
-                className="btn btn-secondary btn-small active"
-                role="button"
-                aria-pressed="true"
-              >
-                ADD
-              </a>
-            </div>
-          </div>
-        </header>
-      </div>
-      <div id="mainPG">
-        {/* <div id="mainPG"> */}
-        <div id="points-display">
-          <div id="display-header">{title}</div>
-          <div id="user-table">
-            <table data-toggle="table" className="table table-bordered">
-              <thead>{header}</thead>
-              <tbody>{details}</tbody>
-            </table>
-          </div>
+const MemberView = ({
+  user, // Will be used when CAS is done
+  pointsReport,
+  getPointsReportForUser,
+  tempUser,
+}) => {
+  //eslint-disable-next-line
+  const [semester, setSemester] = useState("2020spring");
+
+  useEffect(() => {
+    getPointsReportForUser(semester, tempUser);
+    //eslint-disable-next-line
+  }, [semester]);
+
+  var group1Points, group2Points, group3Points, plcPoints;
+  if (pointsReport !== null && pointsReport !== undefined) {
+    // console.log(pointsReport)
+    group1Points = pointsReport[tempUser].group1;
+    group2Points = pointsReport[tempUser].group2;
+    group3Points = pointsReport[tempUser].group3;
+    plcPoints = pointsReport[tempUser].plc;
+  }
+  var group1, group2, group3, plc;
+
+  /* Point requirements
+	Change these values if the points requirements ever change
+	*/
+  var group1Goal = 2;
+  var group2Goal = 1;
+  var group3Goal = 2;
+  var plcGoal = 1;
+
+  if (pointsReport !== null) {
+    if (group1Points <= group1Goal - 2) {
+      group1 = (
+        <div className="red-block">
+          <h4>Group 1</h4>
+          <p>Not Satisfied</p>
         </div>
-      </div>
-    </div>
+      );
+    } else if (group1Points === group1Goal - 1) {
+      group1 = (
+        <div className="yellow-block">
+          <h4>Group 1</h4>
+          <p>1 Hour Remaining</p>
+        </div>
+      );
+    } else {
+      group1 = (
+        <div className="green-block">
+          <h4>Group 1</h4>
+          <p>Satisfied - {group1Points} Hours</p>
+        </div>
+      );
+    }
+
+    if (group2Points <= group2Goal - 1) {
+      group2 = (
+        <div className="red-block">
+          <h4>Group 2</h4>
+          <p>Not Satisfied</p>
+        </div>
+      );
+    } else {
+      group2 = (
+        <div className="green-block">
+          <h4>Group 2</h4>
+          <p>Satisfied - {group2Points} Hour(s)</p>
+        </div>
+      );
+    }
+
+    if (group3Points <= group3Goal - 2) {
+      group3 = (
+        <div className="red-block">
+          <h4>Group 3</h4>
+          <p>Not Satisfied</p>
+        </div>
+      );
+    } else if (group3Points === group3Goal - 1) {
+      group3 = (
+        <div className="yellow-block">
+          <h4>Group 3</h4>
+          <p>1 Hour Remaining</p>
+        </div>
+      );
+    } else {
+      group3 = (
+        <div className="green-block">
+          <h4>Group 3</h4>
+          <p>Satisfied - {group3Points} Hours</p>
+        </div>
+      );
+    }
+
+    if (plcPoints < plcGoal) {
+      plc = (
+        <div className="red-block">
+          <h4>PLC</h4>
+          <p>Not Satisfied</p>
+        </div>
+      );
+    } else {
+      plc = (
+        <div className="green-block">
+          <h4>PLC</h4>
+          <p>Satisfied - {plcPoints} Event(s)</p>
+        </div>
+      );
+    }
+  }
+
+  return (
+    <table>
+      <tbody>
+        <tr>
+          <td>
+            {pointsReport !== null && pointsReport !== undefined && group1}
+          </td>
+        </tr>
+        <tr>
+          <td>
+            {pointsReport !== null && pointsReport !== undefined && group2}
+          </td>
+        </tr>
+        <tr>
+          <td>
+            {pointsReport !== null && pointsReport !== undefined && group3}
+          </td>
+        </tr>
+        <tr>
+          <td>{pointsReport !== null && pointsReport !== undefined && plc}</td>
+        </tr>
+      </tbody>
+    </table>
   );
 };
 
+const mapStateToProps = (state) => ({
+  user: state.user.user,
+  userPointDetails: state.points.userPointDetails,
+  pointsReport: state.points.pointsReport,
+});
+
 MemberView.propTypes = {
-  title: PropTypes.object.isRequired,
-  header: PropTypes.object.isRequired,
-  details: PropTypes.array.isRequired,
+  user: PropTypes.object.isRequired,
+  pointsReport: PropTypes.object.isRequired,
+  getPointsReportForUser: PropTypes.func.isRequired,
+  tempUser: PropTypes.string.isRequired,
 };
 
-export default MemberView;
+export default connect(mapStateToProps, {
+  getPointsReportForUser,
+})(MemberView);
--- client/src/components/Points/Points.jsx
+++ client/src/components/Points/Points.jsx
@@ -1,151 +1,42 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import "./Points.css";
 import { PropTypes } from "prop-types";
 import { getUser } from "../../redux/actions/userActions";
-import {
-  getPointsReport,
-  getUserReport,
-  getPointsDetailForUser,
-  getUserPointsReport,
-} from "../../redux/actions/pointsActions";
 import { connect } from "react-redux";
-import RequirementPoints from "./RequirementPoints";
 import MemberView from "./MemberView";
+import AdminView from "./AdminView";
 
-const Points = ({
-  getUser,
-  user,
-  userPointDetails,
-  getPointsDetailForUser,
-  getPointsReport,
-  getUserPointsReport,
-  //getUserReport,
-  points,
-  pointsReport,
-}) => {
-  const [userChange, setUserChange] = useState({
-    semester: "202002",
-    adminStatus: true,
-  });
-
+const Points = ({ user, getUser }) => {
   const tempUser = "zkang35"; // Not required once user works (getme api call)
   const isAdmin = false; // Not required once user works (getme api call)
 
   useEffect(() => {
     getUser(tempUser);
-    if (user !== null && user !== undefined) {
-      setUserChange({
-        semester: userChange.semester,
-        // adminStatus: user.isAdmin // Add back when user is functional
-        adminStatus: isAdmin, // Used for testing purposes until user is connected
-      });
-    }
-    if (userChange.adminStatus) {
-      getPointsReport(userChange.semester);
-    } else {
-      getPointsDetailForUser(tempUser);
-      getUserPointsReport(userChange.semester, tempUser);
-    }
     //eslint-disable-next-line
-  }, [userChange.semester]);
-
-  // var detailData = userPointDetails;
-  var title = null;
-  var header = null;
-  var details = null;
-
-  title = <div>Member Points Summary</div>;
-
-  details = Object.keys(points).map((username, index) => (
-    <tr className="tr-className-1" data-title="bootstrap table">
-      <td>{username}</td>
-      <td className="td-className-1" data-title="bootstrap table">
-        <a className="date" href="/#">
-          {points[username].semester}
-        </a>
-      </td>
-      <td className="group1">{points[username].group1}</td>
-      <td className="group2">{points[username].group2}</td>
-      <td className="group3">{points[username].group3}</td>
-      <td className="committeeEvents">{points[username].committeeEvents}</td>
-      <td className="plc">{points[username].plc}</td>
-      <td className="aux">{points[username].aux}</td>
-      <td className="officeHours">{points[username].officeHours}</td>
-      <td className="committeeMeetings">
-        {points[username].committeeMeetings}
-      </td>
-    </tr>
-  ));
-  header = (
-    <tr>
-      <th>Member</th>
-      <th>
-        Semester
-        <div className="dropdown">
-          <div>
-            <select
-              onChange={(e) =>
-                setUserChange({
-                  adminStatus: userChange.adminStatus,
-                  semester: e.target.value,
-                })
-              }
-              value={userChange.semester}
-            >
-              <option value=""></option>
-              <option value="201908">Fall 2019</option>
-              <option value="202002">Spring 2020</option>
-              <option value="202005">Summer 2020</option>
-            </select>
-          </div>
-        </div>
-      </th>
-      <th>Category 1 Points</th>
-      <th>Category 2 Points</th>
-      <th>Category 3 Points</th>
-      <th>Committee Points</th>
-      <th>PLC</th>
-      <th>Auxilliary Committee</th>
-      <th>Office Hours</th>
-      <th>Committee Meetings</th>
-    </tr>
-  );
+  }, []);
 
   return (
     <div>
-      {userChange.adminStatus ? (
-        <MemberView title={title} header={header} details={details} />
+      {isAdmin ? (
+        <AdminView />
       ) : (
-        <RequirementPoints
-          userChange={userChange}
-          user={user}
-          tempUser={tempUser}
-          pointsReport={pointsReport}
-        />
+        <MemberView user={user} tempUser={tempUser} />
       )}
     </div>
   );
 };
 
 Points.propTypes = {
+  user: PropTypes.object.isRequired,
   getUser: PropTypes.func.isRequired,
-  // user: PropTypes.object.isRequired,
-  getPointsReport: PropTypes.func.isRequired,
-  getUserPointsReport: PropTypes.func.isRequired,
-  pointsReport: PropTypes.object.isRequired,
 };
 
 const mapStateToProps = (state) => ({
   user: state.user.user,
-  points: state.points.points,
   userPointDetails: state.points.userPointDetails,
   pointsReport: state.points.pointsReport,
 });
 
 export default connect(mapStateToProps, {
   getUser,
-  getPointsReport,
-  getUserReport,
-  getPointsDetailForUser,
-  getUserPointsReport,
 })(Points);
