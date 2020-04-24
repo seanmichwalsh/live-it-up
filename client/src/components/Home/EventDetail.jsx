f6eb68126703be8d1a4cc3fd2cf3c752263ea2ef
Kang, Zixuan <zkang35@gatech.edu>
2020-04-24 12:18:16 -0400

    Updated the Home Page and Trim the time attribute. Update React to 16.12.0

--- client/src/components/Home/EventDetail.jsx
+++ client/src/components/Home/EventDetail.jsx
@@ -1,21 +1,19 @@
-import React, { Component } from 'react';
-import './EventDetail.css';
+import React, { Component } from "react";
+import "./EventDetail.css";
 
 class EventDetail extends Component {
-    render() {
-        return (
-            <div className="eventDetail">
-                <img className="pic"
-                    src={this.props.URL}
-                ></img>
-                <div className="description">
-                    <div>Date: {this.props.date}</div>
-                    <div>Time: {this.props.time} </div>
-                    <div>Location: {this.props.location}</div>
-                </div>
-            </div> 
-        );
-    }
+  render() {
+    return (
+      <div className="eventDetail">
+        <img className="pic" src={this.props.URL} alt=""></img>
+        <div className="description">
+          <div>Date: {this.props.date}</div>
+          <div>Time: {this.props.time} </div>
+          <div>Location: {this.props.location}</div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default EventDetail;
--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,43 +1,72 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
 
-import { getEvents } from '../../redux/actions/eventActions';
+import { getEvents } from "../../redux/actions/eventActions";
 
-import SideBar from './SideBar';
-import EventDetail from './EventDetail';
-import './Home.css';
+import SideBar from "./SideBar";
+import EventDetail from "./EventDetail";
+import "./Home.css";
 
 const Home = ({ getEvents, events }) => {
-  const [ event, setEvent ] = useState([]);
+  //eslint-disable-next-line
+  // const [event, setEvent] = useState([]);
 
   useEffect(() => {
     getEvents();
+    //eslint-disable-next-line
   }, []);
 
   useEffect(() => {
-    events.sort(function(a, b) {
-      var dateA = new Date(a.startTime), dateB = new Date(b.startTime);
+    events.sort((a, b) => {
+      var dateA = new Date(a.startTime),
+        dateB = new Date(b.startTime);
       return dateA - dateB;
     });
-    setEvent(events);
+    // setEvent(events);
   }, [events]);
 
   return (
     <div className="entire-page">
       <div className="main-page">
         <div className="main-header">Upcoming Events</div>
-          <div id="events-container">
-            {events.map((event) => 
-              <EventDetail 
-                URL={process.env.PUBLIC_URL + "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/91873401_10159808570233066_8270082192713973760_o.jpg?_nc_cat=103&_nc_sid=b386c4&_nc_ohc=p1BHxMzZBAEAX8MXlSW&_nc_ht=scontent-lga3-1.xx&oh=651d2f661db11f7086e3f2471429c3d9&oe=5EB67789"}
-                date={event.eventName}
-                time={event.startTime}
-                location={event.location}
-              />
-            )}
-          </div>
-      </div>  
+        <div id="main-event">
+          {events.splice(0, 1).map((event) => (
+            <EventDetail
+              key={event._id}
+              URL={
+                process.env.PUBLIC_URL +
+                "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/91873401_10159808570233066_8270082192713973760_o.jpg?_nc_cat=103&_nc_sid=b386c4&_nc_ohc=p1BHxMzZBAEAX8MXlSW&_nc_ht=scontent-lga3-1.xx&oh=651d2f661db11f7086e3f2471429c3d9&oe=5EB67789"
+              }
+              date={event.eventName}
+              time={
+                event.startTime.substring(0, 10) +
+                " " +
+                event.startTime.substring(11, 19)
+              }
+              location={event.location}
+            />
+          ))}
+        </div>
+        <div id="events-container">
+          {events.splice(1, 4).map((event) => (
+            <EventDetail
+              key={event._id}
+              URL={
+                process.env.PUBLIC_URL +
+                "https://scontent-lga3-1.xx.fbcdn.net/v/t1.0-9/91873401_10159808570233066_8270082192713973760_o.jpg?_nc_cat=103&_nc_sid=b386c4&_nc_ohc=p1BHxMzZBAEAX8MXlSW&_nc_ht=scontent-lga3-1.xx&oh=651d2f661db11f7086e3f2471429c3d9&oe=5EB67789"
+              }
+              date={event.eventName}
+              time={
+                event.startTime.substring(0, 10) +
+                " " +
+                event.startTime.substring(11, 19)
+              }
+              location={event.location}
+            />
+          ))}
+        </div>
+      </div>
       <div className="side-bar">
         <SideBar />
       </div>
@@ -47,11 +76,11 @@ const Home = ({ getEvents, events }) => {
 
 Home.propTypes = {
   getEvents: PropTypes.func.isRequired,
-  events: PropTypes.array.isRequired
+  events: PropTypes.array.isRequired,
 };
 
-const mapStateToProps = state => ({
-  events: state.event.events
+const mapStateToProps = (state) => ({
+  events: state.event.events,
 });
 
 export default connect(mapStateToProps, { getEvents })(Home);
