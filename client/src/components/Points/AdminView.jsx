2ba065232807517d268c8099ef0113b879b53e86
Kang, Zixuan <zkang35@gatech.edu>
2020-04-25 19:55:02 -0400

    Updated React and all React sub libraries. It will cause waring in the browser console if you dont update and Fix the resposivenes for the member view of the points page

--- client/src/components/Points/AdminView.jsx
+++ client/src/components/Points/AdminView.jsx
@@ -2,8 +2,10 @@ import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { connect } from "react-redux";
 import { getPointsReport } from "./../../redux/actions/pointsActions";
+import AdminViewItem from "./AdminViewItem";
+import "./AdminView.css";
 
-const AdminView = ({ points, getPointsReport }) => {
+const AdminView = ({ user, tempUser, points, getPointsReport }) => {
   const [semester, setSemester] = useState("2020spring");
 
   useEffect(() => {
@@ -33,65 +35,46 @@ const AdminView = ({ points, getPointsReport }) => {
       <div id="mainPG">
         <div id="points-display">
           <div id="display-header">
-            <div>Member Points Summary</div>
+            <div>{tempUser}'s Points Summary</div>
           </div>
           <div id="user-table">
             <table data-toggle="table" className="table table-bordered">
               <thead>
-                {
-                  <tr>
-                    <th>Member</th>
-                    <th>
-                      Semester
-                      <div className="dropdown">
-                        <div>
-                          <select
-                            onChange={(e) => setSemester(e.target.value)}
-                            value={semester}
-                          >
-                            <option value=""></option>
-                            <option value="2019fall">Fall 2019</option>
-                            <option value="2020spring">Spring 2020</option>
-                            <option value="2020summer">Summer 2020</option>
-                          </select>
-                        </div>
+                <tr>
+                  <th>Member</th>
+                  <th>
+                    Semester
+                    <div className="dropdown">
+                      <div>
+                        <select
+                          onChange={(e) => setSemester(e.target.value)}
+                          value={semester}
+                        >
+                          <option value=""></option>
+                          <option value="2019fall">Fall 2019</option>
+                          <option value="2020spring">Spring 2020</option>
+                          <option value="2020summer">Summer 2020</option>
+                        </select>
                       </div>
-                    </th>
-                    <th>Category 1 Points</th>
-                    <th>Category 2 Points</th>
-                    <th>Category 3 Points</th>
-                    <th>Committee Points</th>
-                    <th>PLC</th>
-                    <th>Auxilliary Committee</th>
-                    <th>Office Hours</th>
-                    <th>Committee Meetings</th>
-                  </tr>
-                }
+                    </div>
+                  </th>
+                  <th>Category 1 Points</th>
+                  <th>Category 2 Points</th>
+                  <th>Category 3 Points</th>
+                  <th>Committee Points</th>
+                  <th>PLC</th>
+                  <th>Auxilliary Committee</th>
+                  <th>Office Hours</th>
+                  <th>Committee Meetings</th>
+                </tr>
               </thead>
               <tbody>
                 {Object.keys(points).map((username) => (
-                  <tr className="tr-className-1" data-title="bootstrap table">
-                    <td>{username}</td>
-                    <td className="td-className-1" data-title="bootstrap table">
-                      <a className="date" href="/#">
-                        {points[username].semester}
-                      </a>
-                    </td>
-                    <td className="group1">{points[username].group1}</td>
-                    <td className="group2">{points[username].group2}</td>
-                    <td className="group3">{points[username].group3}</td>
-                    <td className="committeeEvents">
-                      {points[username].committeeEvents}
-                    </td>
-                    <td className="plc">{points[username].plc}</td>
-                    <td className="aux">{points[username].aux}</td>
-                    <td className="officeHours">
-                      {points[username].officeHours}
-                    </td>
-                    <td className="committeeMeetings">
-                      {points[username].committeeMeetings}
-                    </td>
-                  </tr>
+                  <AdminViewItem
+                    points={points}
+                    username={username}
+                    key={username}
+                  />
                 ))}
               </tbody>
             </table>
@@ -107,6 +90,8 @@ const mapStateToProps = (state) => ({
 });
 
 AdminView.propTypes = {
+  user: PropTypes.object,
+  tempUser: PropTypes.string.isRequired,
   points: PropTypes.object.isRequired,
   getPointsReport: PropTypes.func.isRequired,
 };
