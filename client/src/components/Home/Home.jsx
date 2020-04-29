57a9c8aeaddaa2cfa3dee83000913d55281156ff
Kang, Zixuan <zkang35@gatech.edu>
2020-04-29 02:16:34 -0400

    Clear Up console and browser warnings and errors

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -1,7 +1,6 @@
 import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import Moment from "moment";
 
 import { getEvents } from "../../redux/actions/eventActions";
 import { getCommittees } from "../../redux/actions/committeeActions";
