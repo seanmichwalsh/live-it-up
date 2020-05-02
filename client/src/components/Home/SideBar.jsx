d83f90e85a01f4d0608eecaea34347082df430c2
BitLeigh Vu <bitleighvu@gatech.edu>
2020-05-01 21:10:24 -0400

    HOME css changes to utilize main styling

--- client/src/components/Home/SideBar.jsx
+++ client/src/components/Home/SideBar.jsx
@@ -30,7 +30,7 @@ const fakeData = [
 
 const SideBar = ({ committees }) => {
     return (
-        <div>
+        <div id="sideBar">
             <div id="gmm-header">
                 <div className="panelHeader">Next GMM</div>
                 <button id="circleIcon">
