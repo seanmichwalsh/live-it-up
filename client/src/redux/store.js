652409460058c85a404ecdbc00751ad4032dd90c
Sean Walsh <seanmichwalsh@protonmail.ch>
2022-01-24 00:24:22 -0500

    Removed redundant packages, updated deprecated packages

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,7 +1,7 @@
 import { createStore, applyMiddleware } from "redux";
 import rootReducer from "./reducers/index";
 import thunk from "redux-thunk";
-import { composeWithDevTools } from "redux-devtools-extension";
+import { composeWithDevTools } from "@redux-devtools/extension";
 
 const initialState = {};
 
