547de610f5ad907ecc074f5ba43bff636761a228
Sean Walsh <seanmichwalsh@protonmail.ch>
2022-04-14 02:49:08 -0400

    Removed unnecessary packages

--- client/test/test.js
+++ client/test/test.js
@@ -1,7 +1,7 @@
 const expect = require('chai').expect
 
-describe('Dummy test to ensure CI/CD works', () => {
-  it('Should be equal', () => {
+describe('Dummy test to ensure CI/CD works', function () {
+  it('Should be equal', function () {
     const variable = 2
     expect(variable).to.equal(2)
   })
